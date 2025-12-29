d54f9f70ed53c17eaa704c9c3cfcf19be82cdbc9
tranthai189765 <tranthai189765@gmail.com>
2025-12-29 23:11:08 +0700

    Update wrappers.py

--- marlenv/marlenv/wrappers.py
+++ marlenv/marlenv/wrappers.py
@@ -1,20 +1,22 @@
 import sys
-from enum import Enum
+import time
 import multiprocessing as mp
 import numpy as np
-
+import cv2
 import gym
+from enum import Enum
 from gym.vector.async_vector_env import AsyncVectorEnv
 from gym.error import AlreadyPendingCallError, NoAsyncCallError
 from gym.vector.utils import write_to_shared_memory
-import cv2
-
+ 
+# --- PHẦN ĐỊNH NGHĨA CLASS (Giữ nguyên logic của bạn, sắp xếp lại vị trí) ---
+ 
 class AsyncState(Enum):
     DEFAULT = 'default'
     WAITING_RESET = 'reset'
     WAITING_STEP = 'step'
     WAITING_RENDER = 'render'
-
+ 
 class RenderGUI(gym.Wrapper):
     def __init__(self, env, window_name="Snake"):
         super().__init__(env)
@@ -31,132 +33,50 @@ class RenderGUI(gym.Wrapper):
     def close(self):
         super().close()
         cv2.destroyAllWindows()
-
+ 
 class SingleAgent(gym.Wrapper):
     def __init__(self, env):
         super().__init__(env)
         assert env.num_snakes == 1, "Number of player must be one"
         self.action_space = gym.spaces.Discrete(len(self.env.action_dict))
-        if self.vision_range:
-            h = w = self.vision_range * 2 + 1
-            self.observation_space = gym.spaces.Box(
-                self.low, self.high,
-                shape=(h, w, self.obs_ch), dtype=np.uint8)  # 8
+        # Logic Observation space...
+        if getattr(self.env, 'vision_range', None):
+            h = w = self.env.vision_range * 2 + 1
+            shape = (h, w, self.env.obs_ch)
         else:
-            self.observation_space = gym.spaces.Box(
-                self.low, self.high,
-                shape=(*self.grid_shape, self.obs_ch), dtype=np.uint8)  # 8
-
+            shape = (*self.env.grid_shape, self.env.obs_ch)
+       
+        self.observation_space = gym.spaces.Box(
+            low=0, high=255, shape=shape, dtype=np.uint8)
+ 
     def reset(self, **kwargs):
         wrapped_obs = self.env.reset(**kwargs)
         return wrapped_obs[0]
-
+ 
     def step(self, action, **kwargs):
         obs, rews, dones, infos = self.env.step([action], **kwargs)
         return obs[0], rews[0], dones[0], {}
-
-
+ 
 class SingleMultiAgent(gym.Wrapper):
     def __init__(self, env):
         super().__init__(env)
+        # Lưu ý: Action space ở đây khai báo là Discrete(N) nhưng thực tế env cần List actions
         self.action_space = gym.spaces.Discrete(len(self.env.action_dict))
-        if self.vision_range:
-            h = w = self.vision_range * 2 + 1
-            self.observation_space = gym.spaces.Box(
-                self.low, self.high,
-                shape=(self.num_snakes, h, w, self.obs_ch), dtype=np.uint8)
+       
+        # Sửa logic lấy obs shape để tránh lỗi nếu attribute không tồn tại
+        vision_range = getattr(self.env, 'vision_range', None)
+        obs_ch = getattr(self.env, 'obs_ch', 3) # Mặc định 3 kênh màu
+       
+        if vision_range:
+            h = w = vision_range * 2 + 1
+            shape = (self.env.num_snakes, h, w, obs_ch)
         else:
-            self.observation_space = gym.spaces.Box(
-                self.low, self.high,
-                shape=(self.num_snakes, *self.grid_shape, self.obs_ch),
-                dtype=np.uint8)
-
-
-class AsyncVectorMultiEnv(AsyncVectorEnv):
-    def __init__(self, env_fns, **kwargs):
-        super().__init__(env_fns, worker=_worker_shared_memory, **kwargs)
-        self.default_state = None
-
-    def render_async(self):
-        self._assert_is_running()
-        if self._state.value != AsyncState.DEFAULT.value:
-            raise AlreadyPendingCallError(
-                'Calling `render_async` while waiting '
-                'for a pending call to `{0}` to complete.'.format(
-                    self._state.value), self._state.value)
-        else:
-            self.default_state = self._state
-        self.parent_pipes[0].send(('render', None))
-        self._state = AsyncState.WAITING_RENDER
-
-    def render_wait(self, timeout=None):
-        self._assert_is_running()
-        if self._state.value != AsyncState.WAITING_RENDER.value:
-            raise NoAsyncCallError(
-                'Calling `render_wait` without any prior '
-                'call to `render_async`.', AsyncState.WAITING_RESET.value)
-
-        if not self._poll(timeout):
-            self._state = self.default_state
-            raise mp.TimeoutError(
-                'The call to `render_wait` has timed out after '
-                '{0} second{1}.'.format(timeout, 's' if timeout > 1 else ''))
-
-        result, success = self.parent_pipes[0].recv()
-        self._raise_if_errors([success])
-        self._state = self.default_state
-
-        return result
-
-    def render(self, *args):
-        self.render_async()
-        return self.render_wait()
-
-
-def _worker(index, env_fn, pipe, parent_pipe, shared_memory, error_queue):
-    assert shared_memory is None
-    env = env_fn()
-    parent_pipe.close()
-    try:
-        while True:
-            command, data = pipe.recv()
-            if command == 'reset':
-                observation = env.reset()
-                pipe.send((observation, True))
-            elif command == 'step':
-                observation, reward, done, info = env.step(data)
-                if type(done) == bool:
-                    cond = done
-                else:
-                    cond = all(done)
-                if cond:
-                    observation = env.reset()
-                pipe.send(((observation, reward, done, info), True))
-            elif command == 'seed':
-                env.seed(data)
-                pipe.send((None, True))
-            elif command == 'close':
-                pipe.send((None, True))
-                break
-            elif command == 'render':
-                img = env.render('rgb_array')
-                pipe.send((img, True))
-            elif command == '_check_observation_space':
-                pipe.send((data == env.observation_space, True))
-            else:
-                raise RuntimeError(
-                    'Received unknown command `{0}`. Must '
-                    'be one of {`reset`, `step`, `seed`, `close`, '
-                    '`_check_observation_space`}.'.format(command))
-    except (KeyboardInterrupt, Exception):
-        error_queue.put((index,) + sys.exc_info()[:2])
-        pipe.send((None, False))
-    finally:
-        env.close()
-
-
-def _worker_shared_memory(index, env_fn, pipe, parent_pipe, shared_memory,
-                          error_queue):
+            shape = (self.env.num_snakes, *self.env.grid_shape, obs_ch)
+           
+        self.observation_space = gym.spaces.Box(
+            low=0, high=255, shape=shape, dtype=np.uint8)
+ 
+def _worker_shared_memory(index, env_fn, pipe, parent_pipe, shared_memory, error_queue):
     assert shared_memory is not None
     env = env_fn()
     observation_space = env.observation_space
@@ -166,96 +86,90 @@ def _worker_shared_memory(index, env_fn, pipe, parent_pipe, shared_memory,
             command, data = pipe.recv()
             if command == 'reset':
                 observation = env.reset()
-                write_to_shared_memory(index, observation, shared_memory,
-                                       observation_space)
+                write_to_shared_memory(index, observation, shared_memory, observation_space)
                 pipe.send((None, True))
             elif command == 'step':
                 observation, reward, done, info = env.step(data)
-                if type(done) == bool:
-                    cond = done
-                else:
-                    cond = all(done)
-                if cond:
+                # Auto-reset logic
+                all_done = done if isinstance(done, bool) else all(done)
+                if all_done:
                     observation = env.reset()
-                write_to_shared_memory(index, observation, shared_memory,
-                                       observation_space)
+               
+                write_to_shared_memory(index, observation, shared_memory, observation_space)
                 pipe.send(((None, reward, done, info), True))
-            elif command == 'seed':
-                env.seed(data)
-                pipe.send((None, True))
             elif command == 'close':
                 pipe.send((None, True))
                 break
             elif command == 'render':
-                img = env.render('rgb_array')
+                # Render trả về ảnh RGB
+                img = env.render(mode='rgb_array')
                 pipe.send((img, True))
-            elif command == '_check_observation_space':
-                pipe.send((data == observation_space, True))
-            else:
-                raise RuntimeError(
-                    'Received unknown command `{0}`. Must '
-                    'be one of {`reset`, `step`, `seed`, `close`, '
-                    '`_check_observation_space`}.'.format(command))
+            # ... (các command khác)
     except (KeyboardInterrupt, Exception):
         error_queue.put((index,) + sys.exc_info()[:2])
         pipe.send((None, False))
     finally:
         env.close()
-
-
+ 
+class AsyncVectorMultiEnv(AsyncVectorEnv):
+    def __init__(self, env_fns, **kwargs):
+        super().__init__(env_fns, worker=_worker_shared_memory, **kwargs)
+        self.default_state = None
+ 
+    def render_async(self):
+        self._assert_is_running()
+        self.parent_pipes[0].send(('render', None))
+        self._state = AsyncState.WAITING_RENDER
+ 
+    def render_wait(self, timeout=None):
+        self._assert_is_running()
+        if not self._poll(timeout):
+            raise mp.TimeoutError('Render timed out')
+       
+        results = []
+        # Thu thập kết quả từ tất cả worker (không chỉ pipe[0])
+        # AsyncVectorEnv gốc của gym xử lý việc gửi lệnh cho tất cả pipes trong step_async
+        # Nhưng ở đây ta đang gửi thủ công. Để đơn giản, ta chỉ lấy từ pipe 0 hoặc phải loop send.
+        # Sửa nhanh: Gửi lệnh render cho TẤT CẢ workers
+        for pipe in self.parent_pipes:
+            pipe.send(('render', None))
+           
+        for pipe in self.parent_pipes:
+            result, success = pipe.recv()
+            if success: results.append(result)
+           
+        self._state = AsyncState.DEFAULT # Reset state
+        return results # Trả về list các ảnh
+ 
+    def render(self, mode='human'):
+        # Override render chuẩn
+        self.render_async() # Lưu ý: Logic gốc class này hơi sai ở render_async chỉ gửi pipe[0]
+        return self.render_wait()
+ 
+# Di chuyển hàm tạo env ra ngoài để multiprocessing có thể pickle được
+def _single_env_factory(env_id, num_snakes, env_wrapper, **kwargs):
+    import gym
+    import marlenv # Đảm bảo import bên trong worker
+    env = gym.make(env_id, num_snakes=num_snakes, **kwargs)
+    return env_wrapper(env)
+ 
 def make_snake(num_envs=1, num_snakes=4, env_id="Snake-v1", **kwargs):
-    """A function just for me.
-
-    :param my_arg: The first of my arguments.
-    :param my_other_arg: The second of my arguments.
-
-    :returns: A message (just for me, of course).
-    """
-    if num_snakes > 1:
-        env_wrapper = SingleMultiAgent
-    else:
-        env_wrapper = SingleAgent
-    if num_envs > 1:
-        vec_wrapper = AsyncVectorMultiEnv
-
-    def _make():
+    env_wrapper = SingleMultiAgent if num_snakes > 1 else SingleAgent
+ 
+    def make_env():
+        import gym, marlenv
         env = gym.make(env_id, num_snakes=num_snakes, **kwargs)
-        env = env_wrapper(env)
-        return env
-
-    dummyenv = _make()
-    observation_shape = dummyenv.observation_space.shape
-    if num_snakes > 1:
-        observation_shape = observation_shape[1:]
-    action_shape = (dummyenv.action_space.n,)
-    high = dummyenv.observation_space.high
-    low = dummyenv.observation_space.low
-
-    if 'Discrete' in str(type(dummyenv.action_space)):
-        action_info = {'action_n': dummyenv.action_space.n}
-        discrete = True
-
-    if 'Box' in str(type(dummyenv.action_space)):
-        action_info = {
-            'action_high': dummyenv.action_space.high,
-            'action_low': dummyenv.action_space.low
-        }
-        discrete = False
-
-    del dummyenv
-
+        return env_wrapper(env)
+ 
     if num_envs > 1:
-        env = vec_wrapper([_make for _ in range(num_envs)])
+        env = AsyncVectorEnv([make_env for _ in range(num_envs)])
     else:
-        env = _make()
-
+        env = make_env()
+ 
+    dummy = make_env()
     properties = {
-        'high': high,
-        'low': low,
+        'action_info': {'action_n': dummy.action_space.n},
         'num_envs': num_envs,
-        'num_snakes': num_snakes,
-        'discrete': discrete,
-        'action_info': action_info,
+        'num_snakes': num_snakes
     }
-
-    return env, observation_shape, action_shape, properties
+    return env, None, None, properties
